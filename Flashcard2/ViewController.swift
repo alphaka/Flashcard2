6f0961dd0406d03666fef2b34e6d3f694ae812a1
alphaka <kabaa346@gmail.com>
2021-03-06 14:30:21 -0500

    Initial Commit

---
+++ Flashcard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard2
+//
+//  Created by Alpha Kabinet Kaba on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
